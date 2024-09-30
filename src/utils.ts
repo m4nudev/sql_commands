bf4e76d32bb8dbca2cc81157e58e5d24c9107976
m4nudev <manudev912@gmail.com>
2024-09-30 19:15:19 +0530

    first commit

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,7 @@
 import { Client } from 'pg';
 
 export async function getClient() {
-    const client = new Client("postgresql://neondb_owner:RAWtHdXFG89U@ep-billowing-recipe-a5wbllc6.us-east-2.aws.neon.tech/neondb?sslmode=require");
+    const client = new Client("-----------------");
     await client.connect();
     return client;
 }
